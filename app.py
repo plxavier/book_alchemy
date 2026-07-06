2608ee587af59ce1fda678a69c809d924a994f0c
Paul Lourdu Xavier <34255718+plxavier@users.noreply.github.com>
2026-07-06 03:25:52 +0200

    fixed port to 5000

--- app.py
+++ app.py
@@ -484,7 +484,7 @@ if __name__ == '__main__':
 
         init_db()
 
-        app.run(host="0.0.0.0", port=5001, debug=True)
+        app.run(host="0.0.0.0", port=5000, debug=True)
     except KeyboardInterrupt:
         print("\nServer stopped by user.")
         logger.info("Server stopped by user")
